38cff8cbc8244f67a21698f48e59d70eeba216ba
Abdul Qoyyum <abdulqoyyum2020@gmail.com>
2022-09-01 22:33:52 +0700

    create new mini library

--- library.js
+++ library.js
@@ -1,81 +1,34 @@
-class Table {
-    constructor(init) {
-      this.init = init;
+class Biodata {
+    nama = ''
+    umur = ''
+    gender = ''
+
+    constructor (nama, umur, gender) {
+        this.nama = nama;
+        this.umur = umur;
+        this.gender = gender;
     }
-  
-    createHeader(data) {
-      let open = "<thead><tr>";
-      let close = "</tr></thead>";
-      data.forEach((d) => {
-        open += `<th>${d}</th>`;
-      });
-  
-      return open + close;
-    }
-  
-    createBody(data) {
-      let open = "<tbody>";
-      let close = "</tbody>";
-  
-      data.forEach((d) => {
-        open += `
-          <tr>
-            <td>${d[0]}</td>
-            <td>${d[1]}</td>
-            <td>${d[2]}</td>
-          </tr>
-        `;
-      });
-  
-      return open + close;
-    }
-  
-    render(element) {
-      let table =
-        "<table class='table table-hover'>" +
-        this.createHeader(this.init.columns) +
-        this.createBody(this.init.data) +
-        "</table>";
-      element.innerHTML = table;
-    }
-  }
-  
-  const table = new Table({
-    columns: ["First Name", "Last Name", "Email"],
-    data: [
-      ["Abdul", "Qoyyum", "abdulqoyyum2020@gmail.com"],
-      ["Edi", "Nugroho", "edinugroho@gmail.com"],
-      ["Joko", "Santoso", "jokosantoso@gmail.com"]
-    ]
-  });
-  const app = document.getElementById("app");
-  table.render(app);
+}
 
+let biodata1 = new Biodata('Abdul', '25', 'Laki-laki');
+let biodata2 = new Biodata('Qoyyum', '25', 'Laki-laki');
+let biodata3 = new Biodata('Sholica', '24', 'Perempuan');
+let nama1 = document.getElementById('nama1');
+let umur1 = document.getElementById('umur1');
+let gender1 = document.getElementById('gender1');
+let nama2 = document.getElementById('nama2');
+let umur2 = document.getElementById('umur2');
+let gender2 = document.getElementById('gender2');
+let nama3 = document.getElementById('nama3');
+let umur3 = document.getElementById('umur3');
+let gender3 = document.getElementById('gender3');
 
-
-
-// class Person {
-//     firstName = ''
-//     lastName = ''
-//     email = ''
-
-//     constructor (firstName, lastName, email) {
-//         this.firstName = firstName;
-//         this.lastName = lastName;
-//         this.email = email;
-//     }
-
-//     render() {
-//         let app = document.getElementById('app');
-        
-//         app.textContent = this.firstName + '    ' + this.lastName + '   ' + this.email;
-//     }
-
-// }
-
-// let person1 = new Person('abdul', 'qoyyum', 'abdulqoyyum2020@gmail.com');
-// let person2 = new Person('adi', 'nugroho', 'adinugroho@gmail.com');
-
-
-// person1.render();
-// person2.render();
+nama1.textContent = biodata1.nama;
+umur1.textContent = biodata1.umur;
+gender1.textContent = biodata1.gender;
+nama2.textContent = biodata2.nama;
+umur2.textContent = biodata2.umur;
+gender2.textContent = biodata2.gender;
+nama3.textContent = biodata3.nama;
+umur3.textContent = biodata3.umur;
+gender3.textContent = biodata3.gender;
